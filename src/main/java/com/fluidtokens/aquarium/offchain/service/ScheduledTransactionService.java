2e72c6d5046fb7584f4f8fe0c3e102a4168b4a70
Giovanni Gargiulo <giovanni@Giovannis-MacBook-Pro.local>
2025-08-29 16:27:39 +0100

    chore: do not attempt to process payments if syncing as it most likely will fail

--- src/main/java/com/fluidtokens/aquarium/offchain/service/ScheduledTransactionService.java
+++ src/main/java/com/fluidtokens/aquarium/offchain/service/ScheduledTransactionService.java
@@ -69,6 +69,8 @@ public class ScheduledTransactionService {
 
     private final AppUtxoService appUtxoService;
 
+    private final BlockEventListener blockEventListener;
+
     private final Vector<TransactionInput> unprocessableScheduledTransactions = new Vector<>();
 
     private final DatumTankConverter datumConverter = new DatumTankConverter();
@@ -87,6 +89,11 @@ public class ScheduledTransactionService {
     public void processPayments() {
 
         log.info("Starting Process Payments RUN");
+
+        if (blockEventListener.getIsSyncing().get()) {
+            log.info("node is syncing, skipping...");
+            return;
+        }
 
         var stakerRefInputOpt = service.findStakerRefInput();
         if (stakerRefInputOpt.isEmpty()) {
